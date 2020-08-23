f02f8b7dce1306bc226a0396ef1ba27a6abdb594
Hector Darkh <d4rkh3xx0r69420@protonmail.com>
2020-08-23 03:42:47 +0300

    made decryption more modular

--- decrypt.py
+++ decrypt.py
@@ -1,11 +1,42 @@
 from argparse import ArgumentParser
 
+from typing import Callable, List, ByteString, NewType
+
 from base64 import b64decode
 
 from Crypto.Hash import SHA1
 from Crypto.Cipher import AES
 
-xorList = ['。', '〃', '〄', '々', '〆', '〇', '〈', '〉', '《', '》', '「', '」', '『', '』', '【', '】', '〒', '〓', '〔', '〕']
+xorList: List[str] = ['。', '〃', '〄', '々', '〆', '〇', '〈', '〉', '《', '》', '「', '」', '『', '』', '【', '】', '〒', '〓', '〔', '〕']
+
+DecryptFunction = NewType('DecryptFunction', Callable[[ByteString, str], ByteString])
+encryption_schemes: List[DecryptFunction] = []
+
+def encryption_scheme(func: DecryptFunction):
+    global encryption_schemes
+    encryption_schemes.append(func)
+
+    return func
+
+@encryption_scheme
+def decrypt_plain(contents: ByteString, key: str) -> ByteString:
+    decryption_key = SHA1.new(data=bytes(key, 'utf-8')).digest()[:16]
+
+    return AES.new(decryption_key, AES.MODE_ECB).decrypt(contents)
+
+@encryption_scheme
+def decrypt_obfuscated(contents: ByteString, key: str) -> ByteString:
+    # unxor the file
+    b64_encrypted_contents = b''
+
+    for index in range(len(contents)):
+        b64_encrypted_contents += bytes([ord(contents[index]) ^ ord(xorList[index % len(xorList)])])
+
+    # base64 decode the file
+    encrypted_contents = b64decode(b64_encrypted_contents)
+
+    # then use the plain decryptor
+    return decrypt_plain(encrypted_contents, key)
 
 # parse arguments
 parser = ArgumentParser('hcdecryptor')
@@ -19,21 +50,6 @@ xor_b64_encrypted_contents = xor_b64_encrypted_file.read()
 
 print(f'Opened file "{args.file}"')
 
-# unxor the file
-print('UnXORing the file')
-b64_encrypted_contents = ''
-
-for index in range(len(xor_b64_encrypted_contents)):
-    b64_encrypted_contents += bytes([ord(xor_b64_encrypted_contents[index]) ^ ord(xorList[index % len(xorList)])])
-
-# base64 decode the file
-print('Decoding the file from Base64')
-encrypted_contents = b64decode(b64_encrypted_contents)
-
-# decrypt the file
-decryption_key = SHA1.new(data=bytes(args.key, 'utf-8')).digest()[:16]
-
-print(f'Decrypting the file with key "{args.key}"')
-original_contents = AES.new(decryption_key, AES.MODE_ECB).decrypt(encrypted_contents)
+original_contents = encryption_schemes[1](xor_b64_encrypted_contents, args.key)
 
 print(original_contents)
